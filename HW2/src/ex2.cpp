3f79392e4c5eeab3b5a9ce23ce89f95e42ee1e7b
DrorPezo <Drorpezo1@gmail.com>
2019-05-10 19:12:15 +0000

    HW2 Update

--- HW2/src/ex2.cpp
+++ HW2/src/ex2.cpp
@@ -11,6 +11,28 @@ ofstream outFile;
 bool is_loop = FALSE;
 int cnt = 0;
 
+typedef struct RtnCount
+{
+    string _name;
+    ADDRINT _address;
+    ADDRINT _tail;
+    RTN _rtn;
+    UINT64 _rtnCount;
+    UINT64 _icount;
+    struct RtnCount * _next;
+} RTN_COUNT;
+
+typedef struct LoopProp
+{
+  UINT64 _itcount;
+  ADDRINT _head;
+  ADDRINT _tail;
+  string _loop_rtn;
+  struct LoopProp *_next;
+} LOOP_PROP;
+
+RTN_COUNT * RtnList = 0;
+LOOP_PROP * LoopList = 0;
 /*****************************************************************************
  *                             Analysis functions                            *
  *****************************************************************************/
@@ -20,6 +42,11 @@ static void count_loops(ADDRINT ip, ADDRINT target)
     loops[target][ip]++;
     is_loop = TRUE;
   }
+}
+
+VOID docount(UINT64 * counter)
+{
+    (*counter)++;
 }
 
 
@@ -41,7 +68,7 @@ static void ins_bbl(BBL bbl)
         IARG_END
       );
       if(is_loop){
-          
+
       }
       is_loop = FALSE;
     }
@@ -57,6 +84,39 @@ static void instrument_trace(TRACE trace, void *v)
   }
 }
 
+VOID Routine(RTN rtn, VOID *v)
+{
+    
+    // Allocate a counter for this routine
+    RTN_COUNT * rc = new RTN_COUNT;
+
+    // The RTN goes away when the image is unloaded, so save it now
+    // because we need it in the fini
+    rc->_name = RTN_Name(rtn);
+    rc->_address = RTN_Address(rtn);
+    rc->_icount = 0;
+    rc->_rtnCount = 0;
+
+    // Add to list of routines
+    rc->_next = RtnList;
+    RtnList = rc;
+            
+    RTN_Open(rtn);
+            
+    // Insert a call at the entry point of a routine to increment the call count
+    RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)docount, IARG_PTR, &(rc->_rtnCount), IARG_END);
+    rc->_tail = INS_Address(RTN_InsTail(rtn));
+    // For each instruction of the routine
+    for (INS ins = RTN_InsHead(rtn); INS_Valid(ins); ins = INS_Next(ins))
+    {
+        // Insert a call to docount to increment the instruction counter for this rtn
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)docount, IARG_PTR, &(rc->_icount), IARG_END);
+    }
+
+    
+    RTN_Close(rtn);
+}
+
 /*****************************************************************************
  *                               Other functions                             *
  *****************************************************************************/
@@ -66,14 +126,42 @@ static void print_results(INT32 code, void *v)
   unsigned long count;
   std::map<ADDRINT, std::map<ADDRINT, unsigned long> >::iterator i;
   std::map<ADDRINT, unsigned long>::iterator j;
-  printf("******* LOOPS *******\n");
   for(i = loops.begin(); i != loops.end(); i++) {
+    LOOP_PROP * lc = new LOOP_PROP;
     target = i->first;
     for(j = i->second.begin(); j != i->second.end(); j++) {
       ip = j->first;
       count = j->second;
-      printf("0x%08jx <- 0x%08jx: %3lu\n", target, ip, count);
+      //printf("0x%08jx <- 0x%08jx: %3lu\n", target, ip, count);
+      lc->_head = target;
+      lc->_tail = ip;
+      lc->_itcount = count;
     } 
+    lc->_next = LoopList;
+    LoopList = lc;
+  }
+  for (RTN_COUNT * rc = RtnList; rc; rc = rc->_next){
+    if (rc->_icount > 0){
+      string rtn_name = rc->_name;
+      ADDRINT head_address = rc->_address;
+      ADDRINT tail_address = rc->_tail;
+      //UINT64 rtn_count = rc->_rtnCount;
+      //UINT64 ins_count = rc->_icount;
+      for (LOOP_PROP * lp = LoopList; lp; lp = lp->_next){
+        ADDRINT loop_head = lp->_head;
+        ADDRINT loop_tail = lp->_tail;
+        if(head_address < loop_head && loop_tail < tail_address){
+          lp->_loop_rtn = rc->_name;
+        }
+      }
+    }
+  }
+  printf("******* LOOPS *******\n");
+  for (LOOP_PROP * lp = LoopList; lp; lp = lp->_next){
+    cout << "Head address: " << showbase << hex << lp->_head <<
+    " Tail address: " << showbase << hex << lp->_tail <<
+    " Iteration count: " << dec << lp->_itcount << 
+    " Routine: " << lp->_loop_rtn << endl;
   }
 }
 
@@ -93,6 +181,7 @@ int main(int argc, char *argv[])
   }
 
   TRACE_AddInstrumentFunction(instrument_trace, NULL);
+  RTN_AddInstrumentFunction(Routine, 0);
   PIN_AddFiniFunction(print_results, NULL);
 
   /* Never returns */
